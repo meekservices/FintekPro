42d7bdf3377c929534f37fce3f6f3b948589613d
meekservices <meekservices@gmail.com>
2026-05-11 22:02:56 +0530

    Await registration of API routesfix: add await to registerRoutes in server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
     });
 
   // 3. Register API Routes
-  const server = registerRoutes(app);
+const server = await registerRoutes(app);
 
   // 4. Global Error Handler
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
